8074153ab2d65db98f3f0d448d72f22bd8a2c209
Eshanboi975 <81173589+Eshanboi975@users.noreply.github.com>
2021-03-30 14:08:55 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 40;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
