147e9dc88ca1d65346a1a2b8389d85e6a4e6d75f
Eshanboi975 <81173589+Eshanboi975@users.noreply.github.com>
2021-03-30 14:14:55 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,10 +9,12 @@ namespace Counter
             Console.WindowHeight = 40;
             for (int i = 1; i <= 10; i++)
             {
+                double result = Math.Pow(2, i);
                 Console.WriteLine(i);
             }
             for (int i = 10; i > 0; i--)
             {
+                double result = Math.Pow(2, i);
                 Console.WriteLine(i);
             }
             Console.ReadKey();
